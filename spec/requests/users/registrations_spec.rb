486110687a44ad6340d704a0b5a874404f116151
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2024-10-07 09:47:50 +0100

    Xit request specs

--- spec/requests/users/registrations_spec.rb
+++ spec/requests/users/registrations_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Users::Registrations", type: :request do
   describe "GET /create" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/users/registrations/create"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/users/registrations/new"
       expect(response).to have_http_status(:success)
     end
