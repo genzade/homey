1679ca21a393e05b4fdff71a55d9dcdd8adb89dc
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2024-10-07 13:30:00 +0100

    Add Current model for use with user session management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def current_user
+    Current.user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
+
+  def user_signed_in?
+    current_user.present?
+  end
+  helper_method :user_signed_in?
+
+  def login(user)
+    Current.user = user
+    reset_session
+    session[:user_id] = user.id
+  end
+
+  def logout
+    Current.user = nil
+    reset_session
+  end
 end
