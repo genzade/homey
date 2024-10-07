a4faa448d2175971fbecbf051aaca3aebdef986c
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2024-10-07 16:08:42 +0100

    Add projects index page (controller + view)

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # root "projects#index"
+  root "projects#index"
 
   namespace :users do
     resource :registration, only: %i[new create]
