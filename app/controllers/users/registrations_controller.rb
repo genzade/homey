856d1f8570e0f8e5257b0a830b2b51bfed24078f
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2024-10-07 16:25:26 +0100

    Add user registration page
    - add basic view page with form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,28 @@
 module Users
   class RegistrationsController < ApplicationController
     def new
+      registration_form = Forms::Users::RegistrationForm.new({})
+
+      render locals: { registration_form: registration_form }
     end
 
     def create
+      registration_form = Forms::Users::RegistrationForm.new(registration_params)
+
+      if registration_form.save
+        login(registration_form.user)
+
+        redirect_to root_path
+      else
+        render :new, locals: { registration_form: registration_form }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def registration_params
+      params.require(:forms_users_registration_form)
+        .permit(:email, :password, :password_confirmation)
     end
   end
 end
