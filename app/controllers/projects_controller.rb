4e8e646aff29798ac34bd25e586f881c0796507f
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2024-10-10 16:27:32 +0100

    Add edit, update actions to project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show edit update]
+
   def index
     @projects = Project.all
 
@@ -6,7 +8,39 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
     render locals: { project: @project }
+  end
+
+  def edit
+    update_project_form = Forms::UpdateProjectForm.new(@project, current_user, {})
+    render locals: { update_project_form: update_project_form, project: @project }
+  end
+
+  def update
+    update_project_form = Forms::UpdateProjectForm.new(@project, current_user, project_params)
+    respond_to do |format|
+      if update_project_form.save
+        flash[:notice] = "Project updated successfully"
+        format.turbo_stream { render :update, locals: { project: @project } }
+      else
+        format.html do
+          render(
+            :edit,
+            locals: { update_project_form: update_project_form, project: @project },
+            status: :unprocessable_entity,
+          )
+        end
+      end
+    end
+  end
+
+  private
+
+  def set_project
+    @project ||= Project.find_by(id: params[:id])
+  end
+
+  def project_params
+    params.require(:forms_update_project_form).permit(:status, :name)
   end
 end
