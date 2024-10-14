210a31bf373bd638d8366578fc7637d34b1fe251
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2024-10-14 20:19:29 +0100

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.find_or_create_by!(email: "test@mail.com") do |user|
+  user.password = "asdfasdf"
+  user.save!
+end
+
+10.times do |i|
+  Project.find_or_create_by!(name: "Super Awesome Project #{i + 1}")
+end
+
+30.times do |i|
+  user = User.find_or_create_by!(email: "test#{i + 1}@mail.com") do |user|
+    user.password = "asdfasdf"
+    user.save!
+
+    project = Project.find(Project.ids.sample)
+    udpate_project_form = Forms::UpdateProjectForm.new(
+      project,
+      user,
+      name: "Just An Awesome Project #{rand(100)}",
+    )
+    udpate_project_form.save
+
+    comment_form = Forms::Projects::CreateCommentForm.new(
+      user,
+      Project.find(Project.ids.sample),
+      body: "comment body",
+    )
+    comment_form.save
+
+    project = Project.find(Project.ids.sample)
+    new_status = (Project.statuses.keys - [ project.status ]).sample
+    udpate_project_form = Forms::UpdateProjectForm.new(
+      project,
+      user,
+      status: new_status,
+    )
+    udpate_project_form.save
+  end
+end
